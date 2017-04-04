aa53192994ed4ca1bea8baf9d195d464a73f6f26
Rashmi Chhabria <resh.10chh@gmail.com>
2017-04-04 17:36:48 +0530

    Database Connectivity

---
+++ constants.php
@@ -0,0 +1,14 @@
+<?php
+	
+	define("DATABASE", "");
+
+	define("SERVER", "localhost");
+
+	define("PASSWORD", "");
+
+	define("USERNAME", "root");
+
+	define("ADMIN_EMAIL", "");
+
+	define("ADMIN_PASSWORD", "");
+?>
